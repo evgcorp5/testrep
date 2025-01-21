bf7bb63f51b826a6d1b1571d8f4762a0bf4a7400
evgcorp5 <avcormevg@gmail.com>
2025-01-21 14:02:50 +0000

    Point

--- stepik/3_1.py
+++ stepik/3_1.py
@@ -1,8 +1,26 @@
-class Constructor:
-    
-    def add_atribute(self, name, value):
-        setattr(self, name, value)
+class Point:
+    def set_coordinates(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_distance_to_origin(self):
+        self._check_coordinates()
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def display(self):
-        print(self.__dict__)
-D
+        if hasattr(self, 'x') and hasattr(self, 'y'):
+            print(f"Point({self.x}, {self.y})")
+        else:
+            print('Координаты не заданы')
+
+    def get_distance(self, obj):
+        if not isinstance(obj, Point):
+            raise TypeError("Передан объект, который не является точкой")
+
+        self._check_coordinates()
+        obj._check_coordinates()
+        return ((self.x - obj.x) ** 2 + (self.y - obj.y) ** 2) ** 0.5
+
+    def _check_coordinates(self):
+        if not hasattr(self, 'x') or not hasattr(self, 'y'):
+            raise AttributeError("Координаты точки не заданы")
