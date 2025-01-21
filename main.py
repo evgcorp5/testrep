3461a2afec7dfc4099bb8ebfaee8c33668cacf26
evgcorp5 <avcormevg@gmail.com>
2025-01-21 07:17:08 +0000

    fix class

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 class Hello:
     hello_msg = 'hello'
 
+    def func(self):
+        print("Hi")
+
 print(Hello.hello_msg)
 
+<<<<<<< HEAD
 
 # it's my test 2 commit
+=======
+>>>>>>> f4127f8 (fix class)
