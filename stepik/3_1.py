f140f6e783b5db74743875f95956617bf52b7937
evgcorp5 <avcormevg@gmail.com>
2025-01-23 07:56:26 +0000

    Write programm to find param of tre.

--- stepik/3_1.py
+++ stepik/3_1.py
@@ -1,7 +1,24 @@
-class Constructor:
-    
-    def add_atribute(self, name, value):
-        setattr(self, name, value)
+class Point:
+    def set_coordinates(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_distance_to_origin(self):
+        if hasattr(self, 'x') and hasattr(self, 'y'):
+            return (self.x ** 2 + self.y ** 2) ** 0.5
+        return None
+
+    def get_distance(self, point2):
+        if not isinstance(point2, Point):
+            print('Передана не точка')
+            return None
+        if hasattr(self, 'x') and hasattr(self, 'y') and hasattr(point2, 'x') and hasattr(point2, 'y'):
+            return (abs(self.x - point2.x) ** 2 + abs(self.y - point2.y) ** 2) ** 0.5
+        print('Координаты не заданы')
+        return None
 
     def display(self):
-        print(self.__dict__)
+        if hasattr(self, 'x') and hasattr(self, 'y'):
+            print(f"Point({self.x}, {self.y})")
+        else:
+            print('Координаты не заданы')
