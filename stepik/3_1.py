a3208af07f89392974863ae18dd9c101f3425bf8
evgcorp5 <avcormevg@gmail.com>
2025-01-23 12:42:30 +0000

    realise stack func

--- stepik/3_1.py
+++ stepik/3_1.py
@@ -1,24 +1,81 @@
-class Point:
-    def set_coordinates(self, x, y):
-        self.x = x
-        self.y = y
+class Stack:
+    def __init__(self):
+        self.values = []
 
-    def get_distance_to_origin(self):
-        if hasattr(self, 'x') and hasattr(self, 'y'):
-            return (self.x ** 2 + self.y ** 2) ** 0.5
-        return None
+    def _check_empty(self):
+        if self.is_empty():
+            print('Empty Stack')
+            return True
+        return False
 
-    def get_distance(self, point2):
-        if not isinstance(point2, Point):
-            print('Передана не точка')
+    def push(self, item):
+        self.values.append(item)
+
+    def pop(self):
+        if self._check_empty():
             return None
-        if hasattr(self, 'x') and hasattr(self, 'y') and hasattr(point2, 'x') and hasattr(point2, 'y'):
-            return (abs(self.x - point2.x) ** 2 + abs(self.y - point2.y) ** 2) ** 0.5
-        print('Координаты не заданы')
-        return None
-
-    def display(self):
-        if hasattr(self, 'x') and hasattr(self, 'y'):
-            print(f"Point({self.x}, {self.y})")
         else:
-            print('Координаты не заданы')
+            return self.values.pop()
+
+    def peek(self):
+        if self._check_empty():
+            return None
+        return self.values[-1]
+
+    def is_empty(self):
+        return len(self.values) == 0
+
+    def size(self):
+        return len(self.values)
+
+
+s = Stack()
+assert s.values == []
+assert isinstance(s, Stack)
+
+s.peek()  # распечатает 'Empty Stack'
+assert s.is_empty() is True
+s.push('cat')
+assert s.size() == 1
+assert s.peek() == 'cat'
+
+s.push('dog')
+assert s.size() == 2
+assert s.peek() == 'dog'
+
+s.push(True)
+assert s.size() == 3
+assert s.is_empty() is False
+
+s.push(777)
+assert s.size() == 4
+
+assert s.pop() == 777
+assert s.size() == 3
+
+assert s.pop() is True
+assert s.size() == 2
+
+s.push(123)
+s.push(123456)
+assert s.peek() == 123456
+assert s.size() == 4
+
+assert s.pop() == 123456
+assert s.pop() == 123
+assert s.pop() == 'dog'
+assert s.is_empty() is False
+assert s.pop() == 'cat'
+assert s.is_empty() is True
+
+
+d = Stack()
+assert d.peek() is None  # Печатает "Empty Stack"
+assert d.pop() is None  # Печатает "Empty Stack"
+d.push('hello')
+assert d.size() == 1
+d.push('world')
+assert d.size() == 2
+assert d.peek() == 'world'
+assert d.pop() == 'world'
+assert d.peek() == 'hello'
